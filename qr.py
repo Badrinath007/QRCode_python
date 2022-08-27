35549a6df6999392f9b7e2d4a64c72b5f077324d
Badrinath007 <63338711+Badrinath007@users.noreply.github.com>
2022-08-27 21:34:39 +0530

    Update qr.py

--- qr.py
+++ qr.py
@@ -2,7 +2,7 @@ import pyqrcode
 import png
 import webbrowser
 from pyqrcode import QRCode
-s="https://www.linkedin.com/in/badrinath- ayyamperumal-1b676a19b/ "
+s="url to be embedded "
 url=pyqrcode.create(s)
 url.svg("myqr.svg",scale=8)
 url.png('myqr.png',scale=8)
