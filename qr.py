bfd906649b355631bcf87521f3bbc77e629e7f12
Badrinath Ayyamperumal <63338711+Badrinath007@users.noreply.github.com>
2023-11-27 11:47:49 +0530

    Update qr.py

--- qr.py
+++ qr.py
@@ -1,8 +1,10 @@
-import pyqrcode
+#Before compiling and executing the below code, Don't forget to install the required packages using pip command.
+
+import pyqrcode #importing pyqrcode,png,webbrowser
 import png
 import webbrowser
 from pyqrcode import QRCode
-s="url to be embedded "
+s="url to be embedded " #The url is stored in the variable s
 url=pyqrcode.create(s)
 url.svg("myqr.svg",scale=8)
 url.png('myqr.png',scale=8)
